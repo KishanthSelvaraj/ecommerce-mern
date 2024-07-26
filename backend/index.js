6cfb2c4576161c536f391ab95947c663e5553851
Kishanth S <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-07-26 14:57:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://ecommerce-mern-server-tau.vercel.app/images/${req.file.filename}`,
+    image_url: `https://ecommerce-mern-w5oy.onrender.com/images/${req.file.filename}`,
   });
 });
 
